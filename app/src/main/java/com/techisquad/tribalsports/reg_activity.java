e9d784df25bcb79c35dcefe0267ead7ef76a10f4
karan kumar <kumar.karankk77@gmail.com>
2018-03-26 15:51:30 +0530

    karan front end

---
+++ app/src/main/java/com/techisquad/tribalsports/reg_activity.java
@@ -0,0 +1,15 @@
+package com.techisquad.tribalsports;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class reg_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.reg_layout_activity);
+
+
+    }
+}
