710bd42b9cf78f3ab50cc05528327c653159340f
NAVITA\NAVITA JAIN <navitajain011@gmail.com>
2018-03-31 04:03:22 +0530

    intent_03

--- app/src/main/java/com/techisquad/tribalsports/log_regis.java
+++ app/src/main/java/com/techisquad/tribalsports/log_regis.java
@@ -1,13 +1,26 @@
 package com.techisquad.tribalsports;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class log_regis extends AppCompatActivity {
-
+        Button lgn_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.log_regis_);
+
+        lgn_btn = findViewById(R.id.button1);
+
+        lgn_btn.setOnClickListener(new View.OnClickListener(){                                  //forgot password page
+            @Override
+            public void onClick(View view){
+                Intent myIntent=new Intent(log_regis.this,login.class);
+                log_regis.this.startActivity(myIntent);
+            }
+        });
     }
 }
--- app/src/main/java/com/techisquad/tribalsports/login.java
+++ app/src/main/java/com/techisquad/tribalsports/login.java
@@ -5,10 +5,13 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class login extends AppCompatActivity {
     Button log_reg;
     Button fog_pwd;
+    Button log_in;
+//    EditText user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +20,8 @@ public class login extends AppCompatActivity {
 
         log_reg=findViewById(R.id.button2);
         fog_pwd=findViewById(R.id.button3);
+        log_in=findViewById(R.id.button1);
+//        user=findViewById(R.id.usrnam);
 
         log_reg.setOnClickListener(new View.OnClickListener(){                                  //register page
             @Override
@@ -33,5 +38,16 @@ public class login extends AppCompatActivity {
                 login.this.startActivity(myIntent);
             }
         });
+
+
+        log_in.setOnClickListener(new View.OnClickListener(){                                  //register page
+            @Override
+//            int id = Integer.parseInt()
+            public void onClick(View view){
+                Intent myIntent=new Intent(login.this,log_regis.class);
+                login.this.startActivity(myIntent);
+            }
+        });
+
     }
 }
